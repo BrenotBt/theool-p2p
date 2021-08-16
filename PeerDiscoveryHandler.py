0a624e842a02ca813546cf60701f67257f01d463
brenorbt <brenorbt@yandex.ru>
2021-08-16 15:18:13 +0400

    modif

--- PeerDiscoveryHandler.py
+++ PeerDiscoveryHandler.py
@@ -12,8 +12,9 @@ class PeerDiscoveryHandler():
     def start(self):
         statusThread = threading.Thread(target=self.status, args=())
         statusThread.start()
-        seedThread = threading.Thread(target=self.connect_to_seed, args=())
-        seedThread.start()
+        if not (self.socketCommunication.seedDiscovery.is_seed(self.socketCommunication.ip)):
+            seedThread = threading.Thread(target=self.connect_to_seed, args=())
+            seedThread.start()
         #discoveryThread = threading.Thread(target=self.discovery, args=())
         #discoveryThread.start()
 
@@ -25,28 +26,34 @@ class PeerDiscoveryHandler():
             time.sleep(5)
 
     def connect_to_seed(self):
-        if not (self.socketCommunication.seedDiscovery.is_seed(self.socketCommunication.ip)):
-            while True:
-                print('PEERREGISTER')
-                self.socketCommunication.seed_ip = self.socketCommunication.seedDiscovery.seed_ip()
-                connected_node = self.socketCommunication.connect_with_node(self.socketCommunication.seed_ip, self.socketCommunication.port)
-                messageType = 'PEERREGISTER'
-                senderConnector = {"ip": self.socketCommunication.ip, "port": self.socketCommunication.port}
-                data = self.socketCommunication.peers
-                message = Message(messageType, senderConnector, data)
-                self.socketCommunication.send(connected_node, message)
-                time.sleep(60)
+        while True:
+            print('PEERREGISTER')
+            self.socketCommunication.connect_with_node(
+                self.socketCommunication.seedDiscovery.seed_ip(),
+                self.socketCommunication.port
+            )
+            time.sleep(60)
 
-    """
+    def handshake(self, connected_node, refresh_seed=False):
+        if refresh_seed or (not (self.socketCommunication.seedDiscovery.is_seed(self.socketCommunication.ip)) and not self.socketCommunication.peers):
+            handshake_message = self.connect_to_seed_message()
+        else:
+            handshake_message = self.handshakeMessage()
+        self.socketCommunication.send(connected_node, handshake_message)
+
     def discovery(self):
         while True:
             handshakeMessage = self.handshakeMessage()
             self.socketCommunication.broadcast(handshakeMessage)
             time.sleep(10)
 
-    def handshake(self, connected_node):
-        handshakeMessage = self.handshakeMessage()
-        self.socketCommunication.send(connected_node, handshakeMessage)
+    def connect_to_seed_message(self):
+        messageType = 'PEERREGISTER'
+        senderConnector = {"ip": self.socketCommunication.ip, "port": self.socketCommunication.port}
+        data = self.socketCommunication.peers
+        message = Message(messageType, senderConnector, data)
+        encodedMessage = BlockchainUtils.encode(message)
+        return encodedMessage
 
     def handshakeMessage(self):
         ownConnector = self.socketCommunication.socketConnector
@@ -55,9 +62,9 @@ class PeerDiscoveryHandler():
         messageType = 'DISCOVERY'
         message = Message(ownConnector, messageType, data)
         encodedMessage = BlockchainUtils.encode(message)
-        print(ownConnector, data, encodedMessage)
         return encodedMessage
-
+    """
+    
     def handleMessage(self, message):
         peersSocketConnector = message.senderConnector
         peersPeerList = message.data
